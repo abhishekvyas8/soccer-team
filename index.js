ff0cf649992a2de2aa419f00bd2741045eaf199d
Abhishek Vyas <vyas6@purdue.edu>
2018-05-16 20:59:59 -0400

    Display form submission as a list

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const handleSubmit = (ev) => {
     ev.preventDefault();
     
     const data = {
-        playerName: ev.target.playerName.value,
-        position: ev.target.position.value,
+        'Player Name': ev.target.playerName.value,
+        'Position': ev.target.position.value,
     }
 
-    renderList(data);
+    const players = document.querySelector('#players');
+    players.appendChild(createList(data));
+    
+    ev.target.reset();
+    ev.target.playerName.focus();
 }
 
-function renderList(data){
-    const list = document.createItem('ul');
+function createList(data){
+    const list = document.createElement('ul');
+    const labels = Object.keys(data);
+    labels.forEach((label) => {
+        list.appendChild(createListItem(label, data[label]));
+    })
+    return list;
+}
+
+function createListItem(label, value){
+    const item = document.createElement('li');
+    item.textContent = `${label}: ${value}`
+    return item;
 }
 
 from.addEventListener('submit', handleSubmit);
